ad8c5f495155bb17216201b47d5a76500f3510f6
Felipe Ramirez <feliperamirezloaiza10@gmail.com>
2026-08-23 23:16:12 -0500

    Modificación de archivo PEP 8

--- ejercicios-clase/semana-02/refactor_pep8.py
+++ ejercicios-clase/semana-02/refactor_pep8.py
@@ -1,8 +1,23 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
- 
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+def calcular_promedio(numeros: list[int | float]) -> float:
+    """Calcula el promedio aritmético de una lista de números.
+
+    Args:
+        numeros (list[int | float]): Lista con valores numéricos.
+
+    Returns:
+        float: El promedio aritmético de los elementos de la lista.
+    """
+    suma = 0
+    for numero in numeros:
+        suma += numero
+    return suma / len(numeros)
+
+
+def main() -> None:
+    lista_numeros = [1, 2, 3, 4, 5]
+    promedio = calcular_promedio(lista_numeros)
+    print(promedio)
+
+
+if __name__ == "__main__":
+    main()
